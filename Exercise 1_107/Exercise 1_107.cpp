d31fb2601259a6fa0671c4fff496c7ce61704086
rezaikram <muhammadikramriza@gmail.com>
2023-07-12 14:35:19 +0700

    Add project files.

---
+++ Exercise 1_107/Exercise 1_107.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int a[20];
+int n;
+
+double rerata(double a, double b) {
+	return (a + b) / 2;
+}
+
+string status(double rata, double nil) {
+	if (rata >= 70 || nil >= 80)
+		return "Lia Lulus";
+	else
+		return "Lia Gagal";
+}
+
+string status2(double rata, double nil) {
+	if (rata >= 70 || nil >= 80)
+		return "Asroni Lulus";
+	else
+		return "Asroni Gagal";
+}
+
+string status3(double rata, double nil) {
+	if (rata >= 70 || nil >= 80)
+		return "Joko Lulus";
+	else
+		return "Joko Gagal";
+}
+
+void display() {
+	cout << endl;
+	cout << "================";
+	cout << "Masukkan Array";
+	cout << "================";
+}
+
+int main() {
+	double nilM, nilB;
+	//double rata;
+	//string st;
+	cout << "Masukan nilai Matematika = ";
+	cin >> nilM;
+	cout << "Masukkan nilai Bahasa Inggris = ";
+	cin >> nilB;
+	//rata = rerata(nilM,nilB);
+	//st = status(rata);
+	//cout << "Status kelulusan = ' << st;
+	//cout << "Status kelulusan = " << status(rata);
+	cout << "Status kelulusan = " << status(rerata(nilM, nilB));
+	return 0;
+}
